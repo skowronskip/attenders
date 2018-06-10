733a523a638762f76fb476398bed3ec5ea0360e1
Pawel Skowronski <210952@edu.p.lodz.pl>
2018-06-10 19:06:04 +0200

    Task [IFETP18MKA-46] Backend for checking attendance - student side

---
+++ server/model/attendanceModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AttendanceSchema = new Schema({
+    creationDate: {
+        type: Date
+    },
+    lecture: {
+        type: Schema.Types.ObjectId,
+        ref: 'lectures'
+    },
+    student: {
+        type: Schema.Types.ObjectId,
+        ref: 'students'
+    }
+});
+
+const Attendance = mongoose.model('attendances', AttendanceSchema);
+
+module.exports = Attendance;
--- server/routes/student.js
+++ server/routes/student.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/attenders');
 mongoose.Promise = global.Promise;
 const User = require('../model/userModel');
+const Lecture = require('../model/lectureModel');
+const Attendance = require('../model/attendanceModel');
 
 router.put('/updateUser', function (req, resp) {
     User.findOne({_id: req.body.id}, function (err, student) {
@@ -21,6 +23,23 @@ router.put('/updateUser', function (req, resp) {
         }
         else {
             resp.status(409).end("User not found");
+        }
+    });
+});
+
+router.post('/checkAttendance', function (req, resp) {
+    Lecture.findOne({$and: [{isOpen: true}, {pin: req.body.pin}]}, function (err, obj) {
+        if (err) throw err;
+        if(obj){
+            var attentance = {};
+            attentance.lecture = obj._id;
+            attentance.student = req.body.id;
+            attentance.creationDate = req.body.moment;
+            Attendance.create(attentance);
+            resp.end();
+        }
+        else {
+            resp.status(409).end("Wrong PIN. Try again!");
         }
     });
 });
